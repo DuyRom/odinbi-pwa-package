32b7ac2b09a89ccec4a4a479e28f1b94038cf06a
duynguyen <duyit192@hotmail.com>
2022-05-31 11:22:35 +0700

    add vendor.css file to theme

--- src/config/pwa.php
+++ src/config/pwa.php
@@ -4,7 +4,6 @@ return [
     'base_domain'			        => 'localhost',
     'prefix'                => '/pwa',
     'icons_path'			         => storage_path('/vendor/odinbi/pwa/src/resources/icons'),
-    'asset_path'			         => storage_path('/vendor/odinbi/pwa/src/resources/assets'),
     'scope'					            => '.',
     'middleware' => 'web',
 ];
