58a024debfc137e676d52174b04ecfef80b716a3
duynguyen <duyit192@hotmail.com>
2022-05-31 11:25:31 +0700

    add vendor.css file to theme

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -64,7 +64,6 @@ if (!function_exists('theme_asset')) {
      */
     function theme_asset($file)
     {
-        // echo base_path() .'/vendor/odinbi/pwa/src/resources/assets/css/'.$file;
-         echo __DIR__.'/../../vendor/odinbi/pwa/src/resources/assets/css/'.$file;
+        echo base_path() .'/../vendor/odinbi/pwa/src/resources/assets/css/'.$file;
     }
 }
