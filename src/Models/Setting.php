aa3f9e1e9c64b300380f778f602d2ef848ce056b
duynguyen <duyit192@hotmail.com>
2022-06-09 14:30:15 +0700

    add database driver to config

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     protected $table = 'pwa_settings';
-
+    protected $connection = config('odb_pwa.database.driver');
     protected $fillable = [
         'tenant_id',
         'data',
