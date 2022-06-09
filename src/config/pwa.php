0bf019f0b756be9e88e6b0e9426ff1a87eaf840b
duynguyen <duyit192@hotmail.com>
2022-06-09 14:35:12 +0700

    add driver database to config

--- src/config/pwa.php
+++ src/config/pwa.php
@@ -6,4 +6,7 @@ return [
     'icons_path'			         => storage_path('/vendor/odinbi/pwa/src/resources/icons'),
     'scope'					            => '.',
     'middleware' => 'web',
+    'database' => [
+    	'driver' => 'mysql'
+    ],
 ];
